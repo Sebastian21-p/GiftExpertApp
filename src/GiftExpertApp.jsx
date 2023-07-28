748803fce0ccdbd5eb4166db3ba8ce65df943df2
SebastianBer <jesussebastianbber@ufps.edu.co>
2023-07-28 11:35:17 -0500

    input

--- src/GiftExpertApp.jsx
+++ src/GiftExpertApp.jsx
@@ -1,15 +1,36 @@
-import React from 'react'
+import  { useState } from 'react'
+import { AddCategory } from './components/AddCategory'
 
 export const GiftExpertApp = () => {
+
+  const [categories, setCategories] = useState(['One punch', 'Dragon ball'])
+
+  const onAddCategory = (newCategory) => {
+    // console.log(newCategory)
+  setCategories( [ newCategory, ...categories ])
+}
+
   return (
     <>
       {/* Titulo */}
         <h1>GiftExpertApp</h1>
 
       {/* Input */}
-        
+        <AddCategory 
+          // setCategories={setCategories}
+          onNewCategory={onAddCategory}
+        />
 
-        {/* Listado */}
+        {/* Listado de gif*/}
+       
+        <ol>
+          {categories.map(category => {
+            return <li key={category}>{category}</li>
+          })
+          }
+          {/* <li></li> */}
+        </ol>
+          {/* Gif Item*/}
     </>
   )
 }
